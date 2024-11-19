0670e69c571f056b8012dd34aa65432fa77fc297
K-MHOON <azmria117@gmail.com>
2024-11-19 22:48:22 +0900

    fix : 디렉터리 구조 변경

---
+++ catalog-service/src/main/java/com/kmhoon/catalogservice/HomeController.java
@@ -0,0 +1,13 @@
+package com.kmhoon.catalogservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "도서 카탈로그에 오신 것을 환영합니다.";
+    }
+}
