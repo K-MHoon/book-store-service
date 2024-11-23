db3eb77bb60f018effdfcb5cdaa5f6f0012ae722
K-MHOON <azmria117@gmail.com>
2024-11-23 18:10:53 +0900

    test : BookService 테스트코드 작성

---
+++ catalog-service/src/test/java/com/kmhoon/catalogservice/service/BookServiceTest.java
@@ -0,0 +1,46 @@
+package com.kmhoon.catalogservice.service;
+
+import com.kmhoon.catalogservice.domain.Book;
+import com.kmhoon.catalogservice.exception.BookAlreadyExistsException;
+import com.kmhoon.catalogservice.exception.BookNotFoundException;
+import com.kmhoon.catalogservice.repository.BookRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class BookServiceTest {
+
+    @Mock
+    private BookRepository bookRepository;
+
+    @InjectMocks
+    private BookService bookService;
+
+    @Test
+    void whenBookToCreateAlreadyExistsThenThrows() {
+        var bookIsbn = "1234561232";
+        var bookToCreate = new Book(bookIsbn, "Title", "Author", 9.90);
+        when(bookRepository.existsByIsbn(bookIsbn)).thenReturn(true);
+        assertThatThrownBy(() -> bookService.addBookToCatalog(bookToCreate))
+                .isInstanceOf(BookAlreadyExistsException.class)
+                .hasMessage(String.format("ISBN [%s] 은 이미 존재합니다.", bookIsbn));
+    }
+
+    @Test
+    void whenBookToReadDoesNotExistThenThrows() {
+        var bookIsbn = "1234561232";
+        when(bookRepository.findByIsbn(bookIsbn)).thenReturn(Optional.empty());
+        assertThatThrownBy(() -> bookService.viewBookDetails(bookIsbn))
+                .isInstanceOf(BookNotFoundException.class)
+                .hasMessage(String.format("ISBN [%s] 을 찾을 수 없습니다.", bookIsbn));
+    }
+
+}
