0197f6cecb0600436b0e3c142e8aad382a6bf108
K-MHOON <azmria117@gmail.com>
2024-11-30 22:20:52 +0900

    feat : Test용 컨트롤러 및 @ConfigurationPropertiesScan 추가

--- catalog-service/src/main/java/com/kmhoon/catalogservice/HomeController.java
+++ catalog-service/src/main/java/com/kmhoon/catalogservice/HomeController.java
@@ -1,13 +1,20 @@
 package com.kmhoon.catalogservice;
 
+import com.kmhoon.catalogservice.config.ConfigProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
+    private final ConfigProperties configProperties;
+
+    public HomeController(ConfigProperties configProperties) {
+        this.configProperties = configProperties;
+    }
+
     @GetMapping("/")
     public String home() {
-        return "도서 카탈로그에 오신 것을 환영합니다.";
+        return configProperties.getGreeting();
     }
 }
