296710187ca7215ad6f0fa06383725af8ff7ab61
K-MHOON <azmria117@gmail.com>
2024-12-02 23:50:01 +0900

    feat : lombok 추가 및 Book 엔티티 업데이트

--- catalog-service/src/main/java/com/kmhoon/catalogservice/domain/Book.java
+++ catalog-service/src/main/java/com/kmhoon/catalogservice/domain/Book.java
@@ -1,24 +1,72 @@
 package com.kmhoon.catalogservice.domain;
 
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Positive;
+import lombok.*;
+import org.hibernate.proxy.HibernateProxy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-public record Book(
+import java.time.Instant;
+import java.util.Objects;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+public class Book {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
+
         @NotBlank(message = "ISBN은 필수 값입니다.")
         @Pattern(regexp = "^([0-9]{10}|[0-9]{13})$", message = "ISBN 검증에 실패했습니다.")
-        String isbn,
+        private String isbn;
 
         @NotBlank(message = "제목은 필수 값입니다.")
-        String title,
+        private String title;
 
         @NotBlank(message = "책 저자는 필수 값입니다.")
-        String author,
+        private String author;
 
         @NotNull(message = "책 가격은 필수 값입니다.")
         @Positive(message = "책 가격은 0 보다 커야합니다.")
-        Double price
+        private Double price;
 
-        ) {
+        private String publisher;
+
+        @CreatedDate
+        private Instant createdDate;
+
+        @LastModifiedDate
+        private Instant lastModifiedDate;
+
+        @Version
+        private int version;
+
+        public static Book of(String isbn, String title, String author, Double price, String publisher) {
+                return new Book(null, isbn, title, author, price, publisher, null, null, 0);
+        }
+
+        @Override
+        public final boolean equals(Object o) {
+                if (this == o) return true;
+                if (o == null) return false;
+                Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+                Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+                if (thisEffectiveClass != oEffectiveClass) return false;
+                Book book = (Book) o;
+                return getId() != null && Objects.equals(getId(), book.getId());
+        }
+
+        @Override
+        public final int hashCode() {
+                return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+        }
 }
