6da8c204fb20ce7e547c285d14d69aa8ee67a0ba
K-MHOON <azmria117@gmail.com>
2024-12-06 23:48:35 +0900

    fix : DataLoader 데이터 리포지토리, BookRepository에 @Transactional 적용

--- catalog-service/src/main/java/com/kmhoon/catalogservice/data/DataLoader.java
+++ catalog-service/src/main/java/com/kmhoon/catalogservice/data/DataLoader.java
@@ -2,26 +2,26 @@ package com.kmhoon.catalogservice.data;
 
 import com.kmhoon.catalogservice.domain.Book;
 import com.kmhoon.catalogservice.repository.BookRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 @Profile("testdata")
+@RequiredArgsConstructor
 public class DataLoader {
 
     private final BookRepository bookRepository;
 
-    public DataLoader(BookRepository bookRepository) {
-        this.bookRepository = bookRepository;
-    }
-
     @EventListener(ApplicationReadyEvent.class)
     public void loadBookTestData() {
-        var book1 = new Book("1234567891", "시바의 모험", "김시바", 11.10);
-        var book2 = new Book("1234567892", "걸리버 시바", "박시바", 13.10);
-        bookRepository.save(book1);
-        bookRepository.save(book2);
+        bookRepository.deleteAll();
+        var book1 = Book.of("1234567891", "시바의 모험", "김시바", 11.10, "김퍼블");
+        var book2 = Book.of("1234567892", "걸리버 시바", "박시바", 13.10, "박퍼블");
+        bookRepository.saveAll(List.of(book1,book2));
     }
 }
--- catalog-service/src/main/java/com/kmhoon/catalogservice/repository/BookRepository.java
+++ catalog-service/src/main/java/com/kmhoon/catalogservice/repository/BookRepository.java
@@ -2,11 +2,14 @@ package com.kmhoon.catalogservice.repository;
 
 import com.kmhoon.catalogservice.domain.Book;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 public interface BookRepository extends CrudRepository<Book, Long> {
     Optional<Book> findByIsbn(String isbn);
     boolean existsByIsbn(String isbn);
+
+    @Transactional
     void deleteByIsbn(String isbn);
 }
